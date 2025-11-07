a8fcdb5dc4f947980851267b43c6cf3a0fbabfad
ashirumah11 <josephanyangu1@gmail.com>
2025-11-07 19:30:11 +0300

    Add my refactor prop drilling using context api project

--- alx-fe-reactjs/alx-react-app-props/src/App.jsx
+++ alx-fe-reactjs/alx-react-app-props/src/App.jsx
@@ -1,24 +1,19 @@
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
-import UserProfile from './components/UserProfile';
+
+// src/App.jsx
+import React from 'react';
+import { UserContext } from './UserContext';
+import ProfilePage from './ProfilePage';
 
 function App() {
+  const userData = { name: "Jane Doe",
+     email: "jane.doe@example.com"
+     };
+
   return (
-    <div>
-      <Header />
-      <MainContent />
-      <UserProfile
-        name="Alice"
-        age="25"
-        bio="Loves hiking and photography"
-      />
-      <Footer />
-    </div>
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
   );
 }
+
 export default App;
-
-
- 
-
