8299f9d70a02e60eb4c49be354296772d9b65dbc
ashirumah11 <111014352+ashirumah11@users.noreply.github.com>
2025-11-08 11:05:28 +0300

    Delete alx-fe-reactjs directory

--- alx-fe-reactjs/alx-react-app-new/src/components/MainContent.jsx
+++
@@ -1,9 +0,0 @@
-function MainContent() {
-  return (
-    <main style = {{ border: '1px solid gray', padding: '10px', margin: '10px' }}> 
-      <p style = {{ backgroundColor: 'white', color: 'red', textAlign: 'center' }}>I love to visit New York, Paris, and Tokyo.</p>
-    </main>
-  );
-}
-
-export default MainContent;
--- alx-fe-reactjs/alx-react-app-props/src/UserContext.js
+++
@@ -1,5 +0,0 @@
-// src/UserContext.js
-import { createContext } from 'react';
-
-// Export a named context so imports are explicit and easy to find in codebase
-export const UserContext = createContext(null);
